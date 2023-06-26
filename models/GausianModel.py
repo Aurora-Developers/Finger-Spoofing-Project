5d66bd860ba95909e4d6eb4e0fe1a4d3766d74af
Pablo Munoz s310308 <s310308@studenti.polito.it>
2023-06-26 08:27:19 +0200

    Added k-fold implementation with all PCA combinations

--- models/GausianModel.py
+++ models/GausianModel.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
 from MLandPattern import MLandPattern as ML
+import scipy
 from tabulate import tabulate
 
-table = []
+tablePCA = []
+tableKFold = []
 headers = ["MVG", "Naive", "Tied Gaussian", "Tied Naive"]
 
 
@@ -34,6 +36,9 @@ def split_db(D, L, fraction, seed=0):
     return (DTR, LTR), (DTE, LTE)
 
 
+# TODO Remove all the testing functions
+
+
 def Generative_models(
     train_attributes, train_labels, test_attributes, prior_prob, test_labels, model
 ):
@@ -58,6 +63,65 @@ def Generative_models(
     return Probabilities, Prediction, accuracy
 
 
+def covariance_within_class(matrix_values, label):
+    """
+    Calculates the average covariance within all the classes in a dataset
+    :param matrix_values: matrix with the values associated to the parameters of the dataset
+    :param label: vector with the label values associated with the dataset
+    :return: a matrix with the total average covariance within each class
+    """
+    class_labels = np.unique(label)
+    within_cov = np.zeros((matrix_values.shape[0], matrix_values.shape[0]))
+    n = matrix_values.size
+    for i in class_labels:
+        centered_matrix = ML.center_data(matrix_values[:, label == i])
+        cov_matrix = ML.covariance(centered_matrix, 1)
+        cov_matrix = np.multiply(cov_matrix, centered_matrix.size)
+        within_cov = np.add(within_cov, cov_matrix)
+    within_cov = np.divide(within_cov, n)
+    return within_cov
+
+
+def covariance_between_class(matrix_values, label):
+    """
+    Calculates the total covariance between all the classes in a dataset
+    :param matrix_values: matrix with the values associated to the parameters of the dataset
+    :param label: vector with the label values associated with the dataset
+    :return: a matrix with the covariance between each class
+    """
+    class_labels = np.unique(label)
+    between_cov = np.zeros((matrix_values.shape[0], matrix_values.shape[0]))
+    N = matrix_values.size
+    m_general = ML.mean_of_matrix_rows(matrix_values)
+    for i in range(len(class_labels)):
+        values = matrix_values[:, label == i]
+        nc = values.size
+        m_class = ML.mean_of_matrix_rows(values)
+        norm_means = np.subtract(m_class, m_general)
+        matr = np.multiply(nc, np.dot(norm_means, norm_means.T))
+        between_cov = np.add(between_cov, matr)
+    between_cov = np.divide(between_cov, N)
+    return between_cov
+
+
+def LDA1(matrix_values, label, m):
+    """
+    Calculates the Lineal Discriminant Analysis to perform dimension reduction
+    :param matrix_values: matrix with the datapoints, with each row being a point
+    :param label: vector with the label values associated with the dataset
+    :param m: number of dimensions of the targeted sub-space
+    :return: the LDA directions matrix (W), and the orthogonal sub-space of the directions (U)
+    """
+    class_labels = np.unique(label)
+    Sw = covariance_within_class(matrix_values, label)
+    Sb = covariance_between_class(matrix_values, label)
+    s, U = scipy.linalg.eigh(Sb, Sw)
+    W = U[:, ::-1][:, 0:m]
+    UW, _, _ = np.linalg.svd(W)
+    U = UW[:, 0:m]
+    return W, U
+
+
 if __name__ == "__main__":
     [full_train_att, full_train_label] = load(
         "/Users/pablomunoz/Desktop/Polito 2023-1/MachineLearning/Project/data/Train.txt"
@@ -65,31 +129,31 @@ if __name__ == "__main__":
 
     priorProb = ML.vcol(np.ones(2) * 0.5)
 
+    ### ------------- PCA WITH 2/3 SPLIT ---------------------- ####
     (train_att, train_label), (test_att, test_labels) = split_db(
         full_train_att, full_train_label, 2 / 3
     )
-
-    table.append(["Full"])
+    tablePCA.append(["Full"])
 
     [MVGprob, MVGpredic, accuracy] = ML.MVG_log_classifier(
         train_att, train_label, test_att, priorProb, test_labels
     )
-    table[0].append(accuracy)
+    tablePCA[0].append(accuracy)
 
     [Naiveprob, Naivepredic, accuracy] = ML.Naive_log_classifier(
         train_att, train_label, test_att, priorProb, test_labels
     )
-    table[0].append(accuracy)
+    tablePCA[0].append(accuracy)
 
     [MVGprob, MVGpredic, accuracy] = ML.TiedGaussian(
         train_att, train_label, test_att, priorProb, test_labels
     )
-    table[0].append(round(accuracy * 100, 2))
+    tablePCA[0].append(round(accuracy * 100, 2))
 
     [Naiveprob, Naivepredic, accuracy] = ML.Tied_Naive_classifier(
         train_att, train_label, test_att, priorProb, test_labels
     )
-    table[0].append(round(accuracy * 100, 2))
+    tablePCA[0].append(round(accuracy * 100, 2))
 
     cont = 1
     for i in reversed(range(10)):
@@ -98,25 +162,68 @@ if __name__ == "__main__":
         P, reduced_train = ML.PCA(train_att, i)
         reduced_test = np.dot(P.T, test_att)
 
-        table.append([f"PCA {i}"])
+        tablePCA.append([f"PCA {i}"])
         for model in headers:
             [MVGprob, MVGpredic, accuracy] = Generative_models(
                 reduced_train, train_label, reduced_test, priorProb, test_labels, model
             )
-            table[cont].append(accuracy)
+            tablePCA[cont].append(accuracy)
         cont += 1
         for j in reversed(range(i)):
             if j < 2:
                 break
-            table.append([f"PCA {i} LDA {j}"])
-            W, _ = ML.LDA1(reduced_train, train_label, j)
+            tablePCA.append([f"PCA {i} LDA {j}"])
+            W, _ = LDA1(reduced_train, train_label, j)
             LDA_train = np.dot(W.T, reduced_train)
             LDA_test = np.dot(W.T, reduced_test)
             for model in headers:
                 [MVGprob, MVGpredic, accuracy] = Generative_models(
                     LDA_train, train_label, LDA_test, priorProb, test_labels, model
                 )
-                table[cont].append(accuracy)
+                tablePCA[cont].append(accuracy)
             cont += 1
+
+    print("PCA with a 2/3 split")
     headers = ["Dimensions"] + headers
-    print(tabulate(table, headers=headers))
+    print(tabulate(tablePCA, headers=headers))
+
+    ### ------------- k-fold with different PCA ---------------------- ####
+    headers = headers[1:]
+    tableKFold.append(["Full"])
+    print(f"Size of dataset: {full_train_att.shape[1]}")
+    k_fold_value = int(input("Value for k partitions: "))
+    for model in headers:
+        [accuracy, model] = ML.k_fold(
+            k_fold_value, full_train_att, full_train_label, priorProb, model
+        )
+        tableKFold[0].append(accuracy)
+
+    cont = 1
+    for i in reversed(range(10)):
+        if i < 2:
+            break
+        P, reduced_train = ML.PCA(full_train_att, i)
+
+        tableKFold.append([f"PCA {i}"])
+        for model in headers:
+            [accuracy, model] = ML.k_fold(
+                k_fold_value, reduced_train, full_train_label, priorProb, model
+            )
+            tableKFold[cont].append(accuracy)
+
+        cont += 1
+        for j in reversed(range(i)):
+            if j < 2:
+                break
+            tableKFold.append([f"PCA {i} LDA {j}"])
+            W, _ = LDA1(reduced_train, full_train_label, j)
+            LDA_train = np.dot(W.T, reduced_train)
+            for model in headers:
+                [accuracy, model] = ML.k_fold(
+                    k_fold_value, LDA_train, full_train_label, priorProb, model
+                )
+                tableKFold[cont].append(accuracy)
+            cont += 1
+
+    print("PCA with k-fold")
+    print(tabulate(tableKFold, headers=headers))
