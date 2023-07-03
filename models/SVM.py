1858c57dc05d5e3d10ad77ba63ffdd26bb56d5a3
Pablo Munoz s310308 <s310308@studenti.polito.it>
2023-07-03 02:54:16 +0200

    Updated model calculations

--- models/SVM.py
+++ models/SVM.py
@@ -50,33 +50,33 @@ if __name__ == "__main__":
         full_train_att, full_train_label, 2 / 3
     )
 
-    tablePCA.append(["Full"])
-    tablePCA[0].append(["Dual SVM"])
+    # tablePCA.append(["Full"])
+    # tablePCA[0].append(["Dual SVM"])
 
     cont = 0
     initial_C = 0.01
     initial_K = 1
-    total_iter = 24
+    total_iter = 9
 
-    for ten in range(3):
-        contrain = initial_C * np.power(10, ten)
-        for j in range(3):
-            k = initial_K * np.power(10, j)
-            tablePCA.append([f"C: {contrain}, K: {k}"])
-            [SPost, Predictions, accuracy] = ML.svm(
-                train_att, train_label, test_att, test_labels, contrain, K=k
-            )
-            cont += 1
-            tablePCA[cont].append(round(accuracy * 100, 2))
-            print(f"{round(cont * 100 / total_iter, 2)}%")
-    cont += 1
+    # for ten in range(3):
+    #     contrain = initial_C * np.power(10, ten)
+    #     for j in range(3):
+    #         k = initial_K * np.power(10, j)
+    #         tablePCA.append([f"C: {contrain}, K: {k}"])
+    #         [SPost, Predictions, accuracy] = ML.svm(
+    #             train_att, train_label, test_att, test_labels, contrain, K=k
+    #         )
+    #         cont += 1
+    #         tablePCA[cont].append(round(accuracy * 100, 2))
+    #         print(f"{round(cont * 100 / total_iter, 2)}%")
+    # cont += 1
     tablePCA.append(["Full"])
     tablePCA[cont].append(["Polynomial"])
     initial_d = 2
     initial_const = 0
     for i in range(3):
         contrain = initial_C * np.power(10, i)
-        d = initial_d + i
+        d = initial_d
         for j in range(2):
             const = initial_const + j
             tablePCA.append([f"C: {contrain} d: {d}, c: {const}"])
@@ -94,53 +94,26 @@ if __name__ == "__main__":
             tablePCA[cont].append(round(accuracy * 100, 2))
             print(f"{round(cont * 100 / total_iter, 2)}%")
     cont += 1
-    tablePCA.append(["Full"])
-    tablePCA[cont].append(["Radial"])
-    initial_gamma = 1
-    for ten in range(3):
-        contrain = initial_C * np.power(10, ten)
-        for j in range(3):
-            gamma = initial_gamma + j
-            tablePCA.append([f"C: {contrain}, gamma: {gamma}"])
-            [SPost, Predictions, accuracy] = ML.svm(
-                train_att,
-                train_label,
-                test_att,
-                test_labels,
-                contrain,
-                gamma=gamma,
-                model="radial",
-            )
-            cont += 1
-            tablePCA[cont].append(round(accuracy * 100, 2))
-            print(f"{round(cont * 100 / total_iter, 2)}%")
-
-    # cont = 1
-    # for i in reversed(range(10)):
-    #     if i < 2:
-    #         break
-    #     P, reduced_train = ML.PCA(train_att, i)
-    #     reduced_test = np.dot(P.T, test_att)
-
-    #     tablePCA.append([f"PCA {i}"])
-
-    #     [modelS, _, accuracy] = ML.binaryRegression(
-    #         reduced_train, train_label, l, reduced_test, test_labels
-    #     )
-    #     tablePCA[cont].append(accuracy)
-    #     cont += 1
-    #     for j in reversed(range(i)):
-    #         if j < 2:
-    #             break
-    #         tablePCA.append([f"PCA {i} LDA {j}"])
-    #         W, _ = ML.LDA1(reduced_train, train_label, j)
-    #         LDA_train = np.dot(W.T, reduced_train)
-    #         LDA_test = np.dot(W.T, reduced_test)
-    #         [modelS, _, accuracy] = ML.binaryRegression(
-    #             LDA_train, train_label, l, LDA_test, test_labels
+    # tablePCA.append(["Full"])
+    # tablePCA[cont].append("Radial")
+    # initial_gamma = 1
+    # for ten in range(3):
+    #     contrain = initial_C * np.power(10, ten)
+    #     for j in range(3):
+    #         gamma = initial_gamma * np.power(10, j)
+    #         tablePCA.append([f"C: {contrain}, gamma: {gamma}"])
+    #         [SPost, Predictions, accuracy] = ML.svm(
+    #             train_att,
+    #             train_label,
+    #             test_att,
+    #             test_labels,
+    #             contrain,
+    #             gamma=gamma,
+    #             model="radial",
     #         )
-    #         tablePCA[cont].append(accuracy)
     #         cont += 1
+    #         tablePCA[cont].append(round(accuracy * 100, 2))
+    #         print(f"{round(cont * 100 / total_iter, 2)}%")
 
     print("PCA with a 2/3 split")
     print(tabulate(tablePCA, headers=headers))
--- models/regression.py
+++ models/regression.py
@@ -49,7 +49,6 @@ if __name__ == "__main__":
     minDCf7_list = []
     minDCffull_list = []
     minDCf8_list = []
-
     path = os.path.abspath("data/Train.txt")
     [full_train_att, full_train_label] = load(path)
 
@@ -57,80 +56,91 @@ if __name__ == "__main__":
 
     # ### ------------- PCA WITH 2/3 SPLIT ---------------------- ####
 
-    # for l in l_list:
-    #     tablePCA=[]
-    #     (train_att, train_label), (test_att, test_labels) = ML.split_db(
-    #         full_train_att, full_train_label, 2 / 3)
+    for l in l_list:
+        tablePCA = []
+        (train_att, train_label), (test_att, test_labels) = ML.split_db(
+            full_train_att, full_train_label, 2 / 3
+        )
 
-    #     tablePCA.append(["Full"])
+        tablePCA.append(["Full"])
 
-    #     [Predictions, SPost, accuracy] = ML.binaryRegression(train_att, train_label, l, test_att, test_labels)
+        [Predictions, SPost, accuracy] = ML.binaryRegression(
+            train_att, train_label, l, test_att, test_labels
+        )
 
-    #     confusion_matrix = ML.ConfMat(Predictions, test_labels)
-    #     DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, pi, Cfn, Cfp)
-    #     (minDCF, _, _) = ML.minCostBayes(SPost, test_labels, pi, Cfn, Cfp)
-    #     minDCffull_list.append(minDCF)
-    #     tablePCA[0].append([accuracy, DCFnorm, minDCF])
+        confusion_matrix = ML.ConfMat(Predictions, test_labels)
+        DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, pi, Cfn, Cfp)
+        (minDCF, _, _) = ML.minCostBayes(SPost, test_labels, pi, Cfn, Cfp)
+        minDCffull_list.append(minDCF)
+        tablePCA[0].append([accuracy, DCFnorm, minDCF])
 
-    #     cont = 1
-    #     for i in reversed(range(10)):
-    #         if i < 2:
-    #             break
-    #         P, reduced_train = ML.PCA(train_att, i)
-    #         reduced_test = np.dot(P.T, test_att)
+        cont = 1
+        for i in reversed(range(10)):
+            if i < 2:
+                break
+            P, reduced_train = ML.PCA(train_att, i)
+            reduced_test = np.dot(P.T, test_att)
 
-    #         tablePCA.append([f"PCA {i}"])
+            tablePCA.append([f"PCA {i}"])
 
-    #         [_, SPost, accuracy] = ML.binaryRegression(reduced_train, train_label, l, reduced_test, test_labels)
-    #         [Predictions, _] = ML.calculate_model(SPost, test_att, "Regression", priorProb, test_labels)
-    #         confusion_matrix = ML.ConfMat(Predictions, test_labels)
-    #         DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, pi, Cfn, Cfp)
-    #         (minDCF, _, _) = ML.minCostBayes(SPost, test_labels, pi, Cfn, Cfp)
+            [_, SPost, accuracy] = ML.binaryRegression(
+                reduced_train, train_label, l, reduced_test, test_labels
+            )
+            [Predictions, _] = ML.calculate_model(
+                SPost, test_att, "Regression", priorProb, test_labels
+            )
+            confusion_matrix = ML.ConfMat(Predictions, test_labels)
+            DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, pi, Cfn, Cfp)
+            (minDCF, _, _) = ML.minCostBayes(SPost, test_labels, pi, Cfn, Cfp)
 
-    #         if i==7:
-    #             minDCf7_list.append(minDCF)
-    #         if i==8:
-    #             minDCf8_list.append(minDCF)
+            if i == 7:
+                minDCf7_list.append(minDCF)
+            if i == 8:
+                minDCf8_list.append(minDCF)
 
-    #         tablePCA[cont].append([accuracy, DCFnorm, minDCF])
-    #         cont += 1
-    #         for j in reversed(range(i)):
-    #             if j < 2:
-    #                 break
-    #             tablePCA.append([f"PCA {i} LDA {j}"])
-    #             W, _ = ML.LDA1(reduced_train, train_label, j)
-    #             LDA_train = np.dot(W.T, reduced_train)
-    #             LDA_test = np.dot(W.T, reduced_test)
-    #             [_, SPost, accuracy] = ML.binaryRegression(LDA_train, train_label, l, LDA_test, test_labels)
-    #             [Predictions, _] = ML.calculate_model(SPost, test_att, "Regression", priorProb, test_labels)
-    #             confusion_matrix = ML.ConfMat(Predictions, test_labels)
-    #             DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, pi, Cfn, Cfp)
-    #             (minDCF, _, _) = ML.minCostBayes(SPost, test_labels, pi, Cfn, Cfp)
-    #             tablePCA[cont].append([accuracy, DCFnorm, minDCF])
-    #             cont += 1
+            tablePCA[cont].append([accuracy, DCFnorm, minDCF])
+            cont += 1
+            for j in reversed(range(i)):
+                if j < 2:
+                    break
+                tablePCA.append([f"PCA {i} LDA {j}"])
+                W, _ = ML.LDA1(reduced_train, train_label, j)
+                LDA_train = np.dot(W.T, reduced_train)
+                LDA_test = np.dot(W.T, reduced_test)
+                [_, SPost, accuracy] = ML.binaryRegression(
+                    LDA_train, train_label, l, LDA_test, test_labels
+                )
+                [Predictions, _] = ML.calculate_model(
+                    SPost, test_att, "Regression", priorProb, test_labels
+                )
+                confusion_matrix = ML.ConfMat(Predictions, test_labels)
+                DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, pi, Cfn, Cfp)
+                (minDCF, _, _) = ML.minCostBayes(SPost, test_labels, pi, Cfn, Cfp)
+                tablePCA[cont].append([accuracy, DCFnorm, minDCF])
+                cont += 1
 
-    #     print(f"PCA with a 2/3 split with lambda={l}")
-    #     print(tabulate(tablePCA, headers=headers))
-    # print(minDCf7_list)
-    # print(minDCffull_list)
-    # print(minDCf8_list)
-    # plt.figure()
-    # # x = np.linspace(0, 10, num=100)
-    # # y1 = np.sin(x)
-    # # y2 = np.cos(x)
+        print(f"PCA with a 2/3 split with lambda={l}")
+        print(tabulate(tablePCA, headers=headers))
+    print(minDCf7_list)
+    print(minDCffull_list)
+    print(minDCf8_list)
+    plt.figure()
+    # x = np.linspace(0, 10, num=100)
+    # y1 = np.sin(x)
+    # y2 = np.cos(x)
 
-    # # # Plot the arrays
-    # plt.plot(np.log(l_list), minDCf7_list)
-    # plt.plot(np.log(l_list), minDCf8_list)
-    # plt.plot(np.log(l_list), minDCffull_list)
+    # # Plot the arrays
+    plt.plot(np.log(l_list), minDCf7_list)
+    plt.plot(np.log(l_list), minDCf8_list)
+    plt.plot(np.log(l_list), minDCffull_list)
 
-    # # # Add labels and a legend
-    # # plt.xlabel('x')
-    # # plt.ylabel('y')
-    # # plt.legend()
+    # # Add labels and a legend
+    # plt.xlabel('x')
+    # plt.ylabel('y')
+    # plt.legend()
 
-    # # # Show the plot
-    # plt.show()
+    # # Show the plot
+    plt.show()
 
     ### K-fold binomial Regression ###
     for l in l_list:
@@ -140,7 +150,7 @@ if __name__ == "__main__":
         k = int(input("Number of partitions: "))
 
         [_, _, accuracy, DCFnorm, minDCF] = ML.k_fold(
-            k, full_train_att, full_train_label, priorProb, "regression", l=l
+            k, full_train_att, full_train_label, priorProb, model="regression", l=l
         )
         tableKFold[0].append([accuracy, DCFnorm, minDCF])
 
@@ -155,7 +165,7 @@ if __name__ == "__main__":
                 full_train_att,
                 full_train_label,
                 priorProb,
-                "regression",
+                model="regression",
                 PCA_m=i,
                 l=l,
             )
@@ -171,7 +181,7 @@ if __name__ == "__main__":
                     full_train_att,
                     full_train_label,
                     priorProb,
-                    "regression",
+                    model="regression",
                     PCA_m=i,
                     LDA_m=j,
                     l=l,
@@ -179,5 +189,5 @@ if __name__ == "__main__":
                 tableKFold[cont].append([accuracy, DCFnorm, minDCF])
                 cont += 1
 
-        print("PCA with k-fold")
-        print(tabulate(tableKFold, headers=headers))
+    print("PCA with k-fold")
+    print(tabulate(tableKFold, headers=headers))
